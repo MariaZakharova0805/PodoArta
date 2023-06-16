82e9abdf1e15d17f6b87274d013c5775b1d3731d
unknown <fraumaria2011@yandex.ru>
2023-06-16 13:55:20 +0300

    new vk icon

--- src/entities/SocialLinksBlock/SocialLinksBlock.tsx
+++ src/entities/SocialLinksBlock/SocialLinksBlock.tsx
@@ -1,11 +1,11 @@
 import c from './SocialLinksBlock.module.css'
 import { ILinkInfo } from "../../shared/inteface";
 import instagram from "../../../public/icons/instagram.svg"
-// import vk from "../../../public/icons/vk.svg"
+import vk from "../../../public/icons/vk.svg"
 import telegram from "../../../public/icons/telegram.svg"
 import whatsapp from "../../../public/icons/whatsapp.svg"
 import instagramW from "../../../public/icons/instagramW.svg"
-// import vkW from "../../../public/icons/vkW.svg"
+import vkW from "../../../public/icons/vkW.svg"
 import telegramW from "../../../public/icons/telegramW.svg"
 import whatsappW from "../../../public/icons/whatsappW.svg"
 import { FC } from 'react';
@@ -15,7 +15,7 @@ type ISocialLinksBlock = {
 }
 
 const LinkInfo: ILinkInfo[] = [
-  // { href: 'https://vk.com/podoarta', id: 'vk', src: vk, srcW: vkW },
+  { href: 'https://vk.com/podoarta', id: 'vk', src: vk, srcW: vkW },
   { href: 'https://www.instagram.com/nagaeva_podoarta', id: 'instagram', src: instagram, srcW: instagramW, tag: "*" },
   { href: 'https://t.me/PodoArta_nagaeva', id: 'telegramm', src: telegram, srcW: telegramW },
   { href: 'https://wa.me/79998747811?text=Здравствуйте,%20пишу%20Вам%20с%20сайта%20https://podo-arta.vercel.app/%0D%0A', id: 'whatsapp', src: whatsapp, srcW: whatsappW },
@@ -27,6 +27,7 @@ const SocialLinksBlock: FC<ISocialLinksBlock> = ({ colorType }) => {
       {LinkInfo.map((link) => {
         return <a key={link.id} href={link.href} target="_blank" className={c.svgblock__item}>
           <span className={c.tagBlock}><img src={colorType === "grey" ? link.src : link.srcW} /><span className={c.tag}>{link.tag}</span></span>
+       
         </a>
       })}
     </div >
