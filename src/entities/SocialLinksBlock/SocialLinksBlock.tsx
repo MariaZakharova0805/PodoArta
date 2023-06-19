3fc1379466a8a2fe9380cf695e7a8e25ded120f3
unknown <fraumaria2011@yandex.ru>
2023-06-19 15:19:17 +0300

    vk link added

--- src/entities/SocialLinksBlock/SocialLinksBlock.tsx
+++ src/entities/SocialLinksBlock/SocialLinksBlock.tsx
@@ -18,7 +18,7 @@ const LinkInfo: ILinkInfo[] = [
   { href: 'https://www.instagram.com/nagaeva_podoarta', id: 'instagram', src: instagram, srcW: instagramW, tag: "*" },
   { href: 'https://t.me/PodoArta_nagaeva', id: 'telegramm', src: telegram, srcW: telegramW },
   { href: 'https://wa.me/79998747811?text=Здравствуйте,%20пишу%20Вам%20с%20сайта%20https://podo-arta.vercel.app/%0D%0A', id: 'whatsapp', src: whatsapp, srcW: whatsappW },
-  { href: '#', id: 'vk', src: vk, srcW: vk_w },
+  { href: 'https://vk.com/podoarta', id: 'vk', src: vk, srcW: vk_w },
 ]
 
 const SocialLinksBlock: FC<ISocialLinksBlock> = ({ colorType }) => {
